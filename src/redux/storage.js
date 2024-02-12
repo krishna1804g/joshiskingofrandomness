d6d76e716db53f81897f3709bcee0e043b049f0e
rahul3003 <rahulkhandke71@gmail.com>
2024-02-12 20:02:16 +0530

    package changes and redux thunk changes

--- src/redux/storage.js
+++ src/redux/storage.js
@@ -1,26 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
-import { persistReducer, persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
-import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import utilsSlice from "./slices/utilsSlice";
 
-
 const persistConfig = {
-    key: 'root',
-    storage,
-}
+  key: "root",
+  storage,
+};
 
-
-const persistedReducer = persistReducer(persistConfig, userReducer)
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
-    reducer: {
-        persistedReducer,
-        utils: utilsSlice
-    },
-    devTools: process.env.NODE_ENV !== 'production',
-    middleware: [thunk]
-})
+  reducer: {
+    persistedReducer,
+    utils: utilsSlice,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
